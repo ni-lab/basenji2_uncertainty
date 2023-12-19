2b4d60978d7c08ff27eca24eca9d810e38417d06
Ruchir Rastogi <ruchir_rastogi@cs.berkeley.edu>
2023-12-19 13:41:14 -0800

    Fix duplicate supported driver

--- notebooks/new_eqtl_analysis/motif_analysis/sasse_drivers/get_driver_fastas.py
+++ notebooks/new_eqtl_analysis/motif_analysis/sasse_drivers/get_driver_fastas.py
@@ -13,6 +13,7 @@ CONTEXT_SIZE = 21
 def read_drivers_df():
     drivers_df = pd.read_csv(DRIVERS_TSV_PATH, sep="\t")
     drivers_df["chr_hg38"] = "chr" + drivers_df["chr_hg38"].astype(str)
+    drivers_df["variant_hg38"] = drivers_df["chr_hg38"] + ":" + drivers_df["LociDriverhg38"].astype(str)
     drivers_df["supported"] = [
         np.sign(ism) == np.sign(beta) 
         for ism, beta in zip(drivers_df["ISM"], drivers_df["eQTL"])
@@ -20,16 +21,16 @@ def read_drivers_df():
     return drivers_df
 
 
-def get_sequences(chroms, positions, fasta):
-    sequences, variants = [], []
-    for chrom, pos in zip(chroms, positions):
-        start_pos = pos - CONTEXT_SIZE // 2
+def get_sequences(variants, fasta):
+    sequences = []
+    for v in variants:
+        chrom, pos = v.split(":")
+        start_pos = int(pos) - CONTEXT_SIZE // 2
         end_pos = start_pos + CONTEXT_SIZE - 1
         seq = fasta[chrom][start_pos - 1: end_pos].seq.upper()
         assert len(seq) == CONTEXT_SIZE
         sequences.append(seq)
-        variants.append(f"{chrom}:{pos}")
-    return sequences, variants
+    return sequences
 
 
 def get_gc_content(sequence: str) -> float:
@@ -47,17 +48,12 @@ def publish_fasta(output_path: str, sequences: list, variants: list):
 
 def main():
     drivers_df = read_drivers_df()
-    supported_drivers_df = drivers_df[drivers_df["supported"]].copy()
-    unsupported_drivers_df = drivers_df[~drivers_df["supported"]].copy()
+    supported_variants = list(set(drivers_df[drivers_df["supported"]]["variant_hg38"].values))
+    unsupported_variants = list(set(drivers_df[~drivers_df["supported"]]["variant_hg38"].values))
 
     fasta = Fasta(FASTA_PATH)
-
-    supported_seqs, supported_variants = get_sequences(
-        supported_drivers_df["chr_hg38"], supported_drivers_df["LociDriverhg38"], fasta
-    )
-    unsupported_seqs, unsupported_variants = get_sequences(
-        unsupported_drivers_df["chr_hg38"], unsupported_drivers_df["LociDriverhg38"], fasta
-    )
+    supported_seqs = get_sequences(supported_variants, fasta)
+    unsupported_seqs = get_sequences(unsupported_variants, fasta)
     supported_seq_gcs = [get_gc_content(seq) for seq in supported_seqs]
     unsupported_seq_gcs = [get_gc_content(seq) for seq in unsupported_seqs]
     print(f"# supported: {len(supported_seqs)}, avg GC: {np.mean(supported_seq_gcs):.3f}")
